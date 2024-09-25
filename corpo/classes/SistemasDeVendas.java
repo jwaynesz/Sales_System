ba2eb07b191318ae56b5ae3321e91d60a1948fde
John Wayne <141192949+jwaynesz@users.noreply.github.com>
2024-09-25 15:21:24 -0300

    Add files via upload

---
+++ corpo/classes/SistemasDeVendas.java
@@ -0,0 +1,14 @@
+
+package classes;
+
+import formulario.frmLogin;
+
+public class SistemasDeVendas {
+
+    public static void main(String[] args) {
+        frmLogin miLogin = new frmLogin();
+        miLogin.setlocationRelativeTo(null);
+        miLogin.setVisible(true);
+    }
+    
+}
